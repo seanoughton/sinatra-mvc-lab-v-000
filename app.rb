ffa45ef24cf19b5892795398bb773921a2bb11e4
Sean Oughton <seanoughton@gmail.com>
2018-03-26 19:23:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/user_input' do
+     erb :user_input
+   end
+
+   post '/user_input' do
+     text_from_user = params[:user_text]
+     #@analyzed_text = TextAnalyzer.new(text_from_user)
+     erb :view_piglatin
+   end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,23 @@
+class PigLatinizer
+
+  attr_reader :text
+
+  def initialize(text="")
+    @text = text.downcase
+  end
+
+  def consonants(text)
+    text_array = text.split("")
+    until text_array[0].scan(/[aeiou]/) != []
+      x = text_array.shift
+      text_array << x
+    end
+    text_array.join()
+  end
+
+  def piglatinize(text)
+    base = consonants(text)
+    base + "a"
+  end
+
+end
